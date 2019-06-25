5220f9e642d746a58311f0374302d9e98e250262
Peter Sims <ragtimesims@gmail.com>
2019-06-25 17:45:49 -0400

    added rc-slider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import Palette from './Palette';
-import { generatePalette } from './ColorHelpers';
-
 import SeedColors from './SeedColors';
+import { generatePalette } from './ColorHelpers';
 
 class App extends Component {
 	render() {
 		console.log(generatePalette(SeedColors[4]));
 		return (
 			<div className="App">
-				<Palette {...SeedColors[2]} />
+				<Palette palette={generatePalette(SeedColors[4])} />
 			</div>
 		);
 	}
--- src/Palette.js
+++ src/Palette.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
 import './Palette.css';
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 class Palette extends Component {
 	render() {
-		const colorBoxes = this.props.colors.map((color) => <ColorBox background={color.color} name={color.name} />);
+		const colorBoxes = this.props.palette.colors[500].map((color) => (
+			<ColorBox background={color.hex} name={color.name} />
+		));
 		return (
 			<div className="Palette">
 				{/*Navbar goes here! */}
