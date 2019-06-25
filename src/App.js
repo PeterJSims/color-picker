ed10f3e2b0c0afc4908a45307d1733746d7946be
Peter Sims <ragtimesims@gmail.com>
2019-06-25 16:50:32 -0400

    tricky colorhelper functions added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import Palette from './Palette';
+import { generatePalette } from './ColorHelpers';
+
 import SeedColors from './SeedColors';
 
 class App extends Component {
 	render() {
+		console.log(generatePalette(SeedColors[4]));
 		return (
 			<div className="App">
 				<Palette {...SeedColors[2]} />
