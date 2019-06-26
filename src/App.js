da3e673a1aaee025ea1a2e67efe7459f11ed41b7
Peter Sims <ragtimesims@gmail.com>
2019-06-26 13:23:41 -0400

    Palette routing

--- src/App.js
+++ src/App.js
@@ -5,17 +5,29 @@ import SeedColors from './SeedColors';
 import { generatePalette } from './ColorHelpers';
 
 class App extends Component {
+	findPalette(id) {
+		return SeedColors.find(function(palette) {
+			return palette.id === id;
+		});
+	}
 	render() {
 		return (
 			<Switch>
-				<Route exact path="/" render={() => <h1>Palette List Goes Here </h1>} />
-				<Route exact path="/palette/:id" render={() => <h1>Individual Palette</h1>} />
+				<Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>} />
+				<Route
+					exact
+					path="/palette/:id"
+					render={(routeProps) => (
+						<Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />
+					)}
+				/>
 			</Switch>
+
+			// <div>
+			//   <Palette palette={generatePalette(seedColors[4])} />
+			// </div>
 		);
 	}
 }
-export default App;
 
-/* <div className="App">
-				<Palette palette={generatePalette(SeedColors[4])} />
-			</div> */
+export default App;
