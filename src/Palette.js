4ca9a0587ee4f4012d6cc2b55ce44c1356d1c32c
Peter Sims <ragtimesims@gmail.com>
2019-06-26 09:39:27 -0400

    navbar component/styling and fontimport

--- src/Palette.js
+++ src/Palette.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
-//REMEMBER TO LOAD CSS IN A SPECIFIC ORDER!
-import 'rc-slider/assets/index.css';
+import Navbar from './Navbar';
 import './Palette.css';
-import Slider from 'rc-slider';
 
 class Palette extends Component {
 	constructor(props) {
@@ -22,9 +20,7 @@ class Palette extends Component {
 		const colorBoxes = colors[level].map((color) => <ColorBox background={color.hex} name={color.name} />);
 		return (
 			<div className="Palette">
-				<div className="slider">
-					<Slider defaultValue={level} min={100} step={100} max={900} onAfterChange={this.changeLevel} />
-				</div>
+				<Navbar level={level} changeLevel={this.changeLevel} />
 				{/*Navbar goes here! */}
 				<div className="Palette-colors">{colorBoxes}</div>
 				{/* A footer will go here */}
