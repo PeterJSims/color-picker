7a7599e2ef4b9cf95b09a2b0870f3ebb665f86da
Peter Sims <ragtimesims@gmail.com>
2019-06-26 11:29:54 -0400

    added basic route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import SeedColors from './SeedColors';
 import { generatePalette } from './ColorHelpers';
 
 class App extends Component {
 	render() {
-		console.log(generatePalette(SeedColors[4]));
 		return (
-			<div className="App">
-				<Palette palette={generatePalette(SeedColors[4])} />
-			</div>
+			<Switch>
+				<Route exact path="/" render={() => <h1>Palette List Goes Here </h1>} />
+				<Route exact path="/palette/:id" render={() => <h1>Individual Palette</h1>} />
+			</Switch>
 		);
 	}
 }
 export default App;
+
+/* <div className="App">
+				<Palette palette={generatePalette(SeedColors[4])} />
+			</div> */
