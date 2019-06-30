e6d2169eb883535bedd64b8bb5e233f640f03b08
Peter Sims <ragtimesims@gmail.com>
2019-06-30 15:02:09 -0400

    palettemetaform created and refactoring

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
+import PaletteMetaForm from './PaletteMetaForm';
 import classNames from 'classnames';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
@@ -60,7 +61,7 @@ class PaletteFormNav extends Component {
 		});
 	}
 	render() {
-		const { classes, open } = this.props;
+		const { classes, open, palettes, handleSubmit } = this.props;
 		const { newPaletteName } = this.state;
 		return (
 			<div className={classes.root}>
@@ -86,19 +87,7 @@ class PaletteFormNav extends Component {
 						</Typography>
 					</Toolbar>
 					<div className={classes.navBtns}>
-						<ValidatorForm onSubmit={() => this.props.handleSubmit(newPaletteName)}>
-							<TextValidator
-								label="Palette Name"
-								name="newPaletteName"
-								value={this.state.newPaletteName}
-								onChange={this.handleChange}
-								validators={[ 'required', 'isPaletteNameUnique' ]}
-								errorMessages={[ 'Enter palette name', 'Name already used' ]}
-							/>
-							<Button variant="contained" color="primary" type="submit">
-								Save Palette
-							</Button>
-						</ValidatorForm>
+						<PaletteMetaForm palettes={palettes} handleSubmit={handleSubmit} />
 						<Link to="/">
 							<Button variant="contained" color="secondary">
 								Go Back
