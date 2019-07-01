4d4b8802f96bb5df50aebf77105eb1ad440ff2e9
Peter Sims <ragtimesims@gmail.com>
2019-07-01 08:36:22 -0400

    added localstorage

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import { generatePalette } from './ColorHelpers';
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { palettes: SeedColors };
+		const savedPalettes = JSON.parse(window.localStorage.getItem('palettes'));
+		this.state = { palettes: savedPalettes || SeedColors };
 		this.savePalette = this.savePalette.bind(this);
 		this.findPalette = this.findPalette.bind(this);
 	}
@@ -20,7 +21,10 @@ class App extends Component {
 		});
 	}
 	savePalette(newPalette) {
-		this.setState({ palettes: [ ...this.state.palettes, newPalette ] });
+		this.setState({ palettes: [ ...this.state.palettes, newPalette ] }, this.syncLocalStorage);
+	}
+	syncLocalStorage() {
+		window.localStorage.setItem('palettes', JSON.stringify(this.state.palettes));
 	}
 	render() {
 		return (
