1750a28a5dee3e8fd4377a4d8a8a8f48a2b02c3c
Peter Sims <ragtimesims@gmail.com>
2019-07-01 20:21:38 -0400

    fix newpaletteform bugs

--- src/styles/DraggableColorBoxStyles.js
+++ src/styles/DraggableColorBoxStyles.js
@@ -1,3 +1,5 @@
+import chroma from 'chroma-js';
+
 import sizes from './sizes';
 const styles = {
 	root: {
@@ -28,10 +30,10 @@ const styles = {
 	boxContent: {
 		position: 'absolute',
 		padding: '10px',
+		color: (props) => (chroma(props.color).luminance() <= 0.08 ? 'rgba(255, 255, 255, 0.85)' : 'rgba(0, 0, 0, 0.6'),
 		width: '100%',
 		left: '0px',
 		bottom: '0px',
-		color: 'rgba(0, 0, 0, 0.5)',
 		letterSpacing: '1px',
 		textTransform: 'uppercase',
 		fontSize: '12px',
